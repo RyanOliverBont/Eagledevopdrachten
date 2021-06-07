cfddec1f404f690a9605fdb759f642a8e72ed01e
RyanOliverBont <70368767+RyanOliverBont@users.noreply.github.com>
2021-06-07 23:34:47 +0200

    Add files via upload

---
+++ school/PHPBASIC/opdracht8/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<body>
+<?php
+$voornaam1 = "Sophie";
+$voornaam2 = "Max";
+$voornaam3 = "Kabouter";
+
+echo $voornaam1;
+echo "<br>";
+echo $voornaam2;
+echo "<br>";
+echo $voornaam3;
+?>
+</body>
+</html>
